e9745ffc2a8b0630180d01f6dcee2f7dcc8fb232
Victor Olaoye <olaoyevictor451@gmail.com>
2022-01-18 02:06:10 +0100

    Dispatching Actions

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import { Route } from 'react-router';
 import './index.css';
 import Routes from './Routes'
+import { Provider } from 'react-redux'
+import { Store } from 'redux'
+import configureStore from './Store'
+import { IApplicationState } from './Store'
 
+interface IProps {
+  store: Store<IApplicationState>
+}
+const Root: React.FC<IProps> = props => {
+  return (
+    <Provider store={props.store}>
+      <Route />
+    </Provider>
+  )
+}
+
+const store = configureStore()
 ReactDOM.render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>,
+  <Root store={store} />,
   document.getElementById('root')
 );
